a1c7ded48f94e72b4520fd76bb1921c5a6a12662
MKoussa <matthew.koussa@gmail.com>
2016-04-16 23:52:09 -0500

    Add 	Added mountains, as well as formatted script to look a lot 	prettier. Oh and fixed a minor bug because when you call echo, you 	need to pass in a string!

--- PowerUserFind.ps1
+++ PowerUserFind.ps1
@@ -1,23 +1,41 @@
-﻿echo
-echo "********************************************************************"
+﻿echo ""
+echo "          /\                                       _         _      "
+echo "         /**\                                     |_|  | |  |_      "
+echo "        /****\   /\                               |  . |_|. |  .    "
+echo "       /      \ /**\                                                "
+echo "      /  /\    /    \        /\    /\  /\      /\            /\/\/\ "
+echo "     /  /  \  /      \      /  \/\/  \/  \  /\/  \/\  /\  /\/ / /  \"
+echo "    /  /    \/ /\     \    /    \ \  /    \/ /   /  \/  \/  \  /    "  
+echo "   /  /      \/  \/\   \  /      \    /   /    \                    "
+echo "__/__/_______/___/__\___\___________________________________________"
+echo "*                                                                  *"
+echo "*                  P O W E R - U S E R - F I N D                   *"
+echo "*                                                                  *"
 echo "*Find the computers a user has logged into between a specific range*"
-echo "*Must be run on the AD DC to work, or a PSSession into the AD      *"
-echo "*Date format MUST BE in mm/dd/yyyy HH:MM:SS AM/PM                  *"
-echo "*Example: 02/13/2014 10:40:14 PM                                   *"
-echo "*No Warranty or guarantees. This can take a long time to complete  *"
-echo "*                      by Matthew Koussa                           *"
+echo "*                                                                  *"
+echo "*   Must be run on the AD DC to work, or a PSSession into the AD   *"
+echo "*                                                                  *"
+echo "*        Date format MUST BE in mm/dd/yyyy HH:MM:SS AM/PM          *"
+echo "*                Example: 02/13/2014 10:40:14 PM                   *"
+echo "*                                                                  *"
+echo "* No Warranty or guarantees. This can take a long time to complete *"
+echo "*                                                                  *"
+echo "*                        by Matthew Koussa                         *"
+echo "*                                                                  *"
 echo "********************************************************************"
-echo 
+echo ""
 
 $domainName = Read-Host -Prompt 'Enter Domain Name'
-
+echo ""
 $firstOU = Read-Host -Prompt 'Enter OU'
-
+echo ""
 $userName = Read-Host -Prompt 'Enter the username'
-
+echo ""
 $startDate = Read-Host -Prompt 'Enter the Start Date'
-
+echo ""
 $endDate = Read-Host -Prompt 'Enter the End Date'
+echo ""
+echo "Starting Search..."
 
 $Computers = Get-ADComputer -filter * -Searchbase "OU=$firstOU,DC=$domainName,DC=local" | % {$_.Name}
 
